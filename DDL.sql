3505926b6a16084ba30cd81eb209e4f226ea7997
Troy Lopez <lopeztr@flip4.engr.oregonstate.edu>
2025-02-01 13:37:00 -0800

    reorder + reformat to allow painless sourcing

--- DDL.sql
+++ DDL.sql
@@ -1,44 +1,44 @@
+--- Create Authors Table
+CREATE OR REPLACE TABLE Authors (
+    author_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    biography TEXT
+);
+
+--- Create Genres Table
+CREATE OR REPLACE TABLE Genres (
+    genre_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    genre_name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
 --- Create Books Table
-CREATE or REPLACE TABLE Books (
-    book_ID INT PRIMARY KEY Auto_Increment,
+CREATE OR REPLACE TABLE Books (
+    book_ID INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     author_ID INT NOT NULL,
     synopsis TEXT,
-    audience ENUM("Youth", "Middle-grade", "YA"," Adult", "Misc"),
+    audience ENUM('Youth', 'Middle-grade', 'YA', 'Adult', 'Misc'),
     language VARCHAR(255) NOT NULL,
-    format ENUM("Hardcover", "Paperback", "Ebook", "Audio", "Large Print"),
+    format ENUM('Hardcover', 'Paperback', 'Ebook', 'Audio', 'Large Print'),
     publishing_date DATE,
     FOREIGN KEY (author_ID) REFERENCES Authors(author_ID)
 );
 
---- Create Authors Table
-CREATE or REPLACE TABLE Authors (
-    author_ID INT NOT NULL PRIMARY KEY Auto_Increment,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    biography TEXT,
-);
-
---- Patrons Table
-CREATE or REPLACE TABLE Patrons (
-    patron_ID INT NOT NULL PRIMARY KEY Auto_Increment,
+--- Create Patrons Table
+CREATE OR REPLACE TABLE Patrons (
+    patron_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     email VARCHAR(255),
-    phone_number VARCHAR(16), 
+    phone_number VARCHAR(16)
 );
 
---- Genres Table
-CREATE or REPLACE TABLE Genres (
-    genre_ID INT NOT NULL PRIMARY KEY Auto_Increment,
-    genre_name VARCHAR(255) NOT NULL,
-    description TEXT,
-);
-
---- checkouts
-CREATE or REPLACE TABLE Checkouts (
-    checkout_ID INT NOT NULL PRIMARY KEY Auto_Increment,
+--- Create Checkouts Table
+CREATE OR REPLACE TABLE Checkouts (
+    checkout_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     book_ID INT NOT NULL,
     patron_ID INT NOT NULL,
     checkout_date DATE NOT NULL,
@@ -49,8 +49,8 @@ CREATE or REPLACE TABLE Checkouts (
 );
 
 --- Create Book_Genres Table (intersection)
-CREATE or REPLACE TABLE Book_Genres (
-    book_genre_ID: INT NOT NULL PRIMARY KEY Auto_Increment,
+CREATE OR REPLACE TABLE Book_Genres (
+    book_genre_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     book_ID INT NOT NULL,
     genre_ID INT NOT NULL,
     FOREIGN KEY (book_ID) REFERENCES Books(book_ID),
