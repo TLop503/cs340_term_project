10073ae5b1214605c6ad2461e7164dee70b5b18c
Lorelei Klohr <lorelei@klohr.com>
2025-02-25 16:54:40 -0800

    Read for Patrons

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ def checkout():
 def books():
     return render_template('books.html')
 
+# Genres
+
 @app.route('/genre')
 def genre():
     q = 'SELECT * FROM Genres'
@@ -71,9 +73,56 @@ def edit_genre():
         cur.close()
     return redirect(url_for('genre')) # send user back to genre page
 
+# Patrons
+
 @app.route('/patron')
 def patron():
-    return render_template('patron.html')
+    q = 'SELECT * FROM Patrons'
+    c = mysql.connection.cursor()
+    c.execute(q)
+    results = c.fetchall()
+    return render_template('patron.html', patrons=results)
+
+@app.route('/addPatron', methods=['POST'])
+def add_patron():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        date_of_birth = request.form['date_of_birth']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Patrons (first_name, last_name, date_of_birth, email, phone_number) VALUES (%s, %s, %s, %s, %s);", (first_name, last_name, date_of_birth, email, phone_number))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('patron'))
+
+@app.route('/editPatron', methods=['POST'])
+def edit_patron():
+    if request.method == 'POST':
+        patron_ID = request.form['patron_ID']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        date_of_birth = request.form['date_of_birth']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE Patrons SET first_name=%s, last_name=%s, date_of_birth=%s, email=%s, phone_number=%s WHERE patron_ID=%s;", (first_name, last_name, date_of_birth, email, phone_number, patron_ID))
+        mysql.connection.commit() # ensure our changes save
+        cur.close()
+    return redirect(url_for('patron')) # send user back to patrons page
+
+@app.route('/remPatron', methods=['POST'])
+def rem_patron():
+    if request.method == 'POST':
+        patron_ID = request.form['patron_ID']
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM Patrons WHERE patron_ID=%s;", (patron_ID))
+        mysql.connection.commit() # ensure our changes save
+        cur.close()
+    return redirect(url_for('patron'))
+
+# Authors
 
 @app.route('/author')
 def author():
@@ -116,7 +165,7 @@ def edit_author():
         cur.execute("UPDATE Authors SET first_name=%s, last_name=%s, biography=%s WHERE author_ID=%s;", (new_first_name, new_last_name, new_biography, author_id))
         mysql.connection.commit() # ensure our changes save
         cur.close()
-    return redirect(url_for('author')) # send user back to genre page
+    return redirect(url_for('author')) # send user back to authors page
 
 
 @app.route('/book_genre')
